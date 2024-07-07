12f44dcc68478fdfbcce5ce73e811aee914c6a95
vaibhav23244 <vv200325803@gmail.com>
2024-07-07 07:50:17 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -13,15 +13,24 @@ app.use(cors());
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
-app.get("/", (req, res, next) => {
+app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch((err) => {
-    console.log(err);
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Remove the app.listen call for Vercel deployment
+// Instead, export the Express app
+module.exports = app;
+
+// For local development, you can use this:
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
+}
